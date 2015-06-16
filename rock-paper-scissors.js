0c8ba55945634b996c81fb22f2578bbbeb529afb
Kelly Murray <kelly@devbootcamp.com>
2015-06-16 09:57:39 -0400

    adding if/elseif/else

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,4 +12,32 @@ if (computerChoice <= 0.33) {
     computerChoice = "scissors";
 }
 
-console.log(computerChoice);
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+    return "The result is a tie!";
+} else if(choice1 ==="rock") {
+    if (choice2==="scissors") {
+    return "rock wins";
+} else {
+    return "paper wins";
+        }
+    }
+   else if(choice1==="paper") {
+       if(choice2 ==="rock") {
+           return "paper wins";
+       } else {
+           return "scissors wins";
+       }
+   }
+
+    else if(choice1==="scissors") {
+        if (choice2==="rock") {
+            return "rock wins";
+        } else {
+            return "scissors wins";
+        }
+    }
+
+};
+
+compare(userChoice, computerChoice);
