7cd8bdd78a813d89d36f1516e1436789b4aa345e
Kelly Murray <kellymurray@users.noreply.github.com>
2016-05-11 13:59:55 -0400

    correct typo

--- simple-build-a-story.js
+++ simple-build-a-story.js
@@ -6,6 +6,6 @@ var myVerbs = prompt("Please give me a verb.");
 var myAdjs = prompt("Please give me an adjective.");
 
 //join variables into the story
-var myStory = "Once upon a time there was a " + myAdjs + "and brave " + myNouns + " who decided to " + myVerbs + "!";
+var myStory = "Once upon a time there was a " + myAdjs + " and brave " + myNouns + " who decided to " + myVerbs + "!";
 
 document.write(myStory);
